eea90d86550cc4bbf664c4265497237df0d1f226
Chris Johansson <chris.johansson@mildmedia.se>
2024-12-22 13:07:00 +0100

    Add minor tweaks

--- src/hooks/useBankId.ts
+++ src/hooks/useBankId.ts
@@ -2,7 +2,6 @@
 
 import { useEffect, useState, useCallback } from "react";
 import QRCode from "qrcode";
-import { setDefaultResultOrder } from "dns";
 
 const REFRESH_INTERVAL = 2000; // Refresh every 2000 milliseconds (2 seconds)
 const SIGNUP_TIMEOUT = 30000; // Timeout after 30000 milliseconds (30 seconds)
@@ -10,15 +9,21 @@ const SIGNUP_TIMEOUT = 30000; // Timeout after 30000 milliseconds (30 seconds)
 const QR_CODE = {
     fetchAndUpdateQRCode: async (
         qrStartToken: string,
+        orderRef: any,
         setHasError: any,
         setQRDataString: (data: string) => void,
         startDate: Date
     ) => {
         try {
+            if (!qrStartToken || !startDate) {
+                return;
+            }
+
             const params = new URLSearchParams({
                 method: "refresh-qr",
                 params: JSON.stringify({
                     qrStartToken,
+                    orderRef,
                     startDate: startDate.toString(),
                 }),
             });
@@ -38,12 +43,13 @@ const QR_CODE = {
 
             setQRDataString(data);
         } catch (error: any) {
-            //console.error("Error fetching QR code data:", error.message);
+            //console.error("Error fetching QR code data:", error);
             setHasError(true);
         }
     },
     startQRCodeRefresh: (
         qrStartToken: string,
+        orderRef: string,
         setQRDataString: (data: string) => void,
         setHasError: any,
         completed: boolean,
@@ -53,12 +59,12 @@ const QR_CODE = {
         let startDate = new Date();
 
         const intervalId = setInterval(() => {
-            QR_CODE.fetchAndUpdateQRCode(qrStartToken, setHasError, setQRDataString, startDate);
-
             if (completed || hasTimedOut || hasError) {
                 clearInterval(intervalId);
                 return;
             }
+
+            QR_CODE.fetchAndUpdateQRCode(qrStartToken, orderRef, setHasError, setQRDataString, startDate);
         }, REFRESH_INTERVAL);
 
         return () => clearInterval(intervalId);
@@ -81,6 +87,7 @@ const useBankId = (options?: BankIdOptions) => {
     const [qrDataString, setQRDataString] = useState<string>("");
     const [identification, setIdentification] = useState<any>(null);
     const [autoSignIn, setAutoSignIn] = useState(false);
+    const [hintCode, setHintCode] = useState<string>("");
 
     const reset = useCallback(() => {
         setQRCodeImage("");
@@ -99,7 +106,8 @@ const useBankId = (options?: BankIdOptions) => {
 
     // Init the auth/sign process
     useEffect(() => {
-        if (triggerCounter === 0) return;
+        if (auth || triggerCounter === 0) return;
+        //if (triggerCounter === 0) return;
 
         setHasTimedOut(false);
 
@@ -108,7 +116,7 @@ const useBankId = (options?: BankIdOptions) => {
         }, SIGNUP_TIMEOUT);
 
         const fetchData = async () => {
-            let params: { method: any, userVisibleData?: any } = { method: options?.bankIdMethod || "auth" };
+            let params: { method: any; userVisibleData?: any } = { method: options?.bankIdMethod || "auth" };
 
             if (options?.userVisibleData) {
                 params.userVisibleData = options.userVisibleData;
@@ -116,25 +124,47 @@ const useBankId = (options?: BankIdOptions) => {
 
             const queryString = new URLSearchParams(params).toString();
 
+            const MAX_RETRIES = 5; // Maximum number of retries
+            const RETRY_DELAY = 1000; // Delay between retries in milliseconds
+
+            const retryFetch = async (attempt: number = 1): Promise<any> => {
+                try {
+                    const response = await fetch(`/api/bankId?${queryString}`, {
+                        method: "GET",
+                        headers: {
+                            "Content-Type": "application/json",
+                            'x-api-key': process.env.NEXT_PUBLIC_BANKID_VERIFICATION_API_KEY || "",
+                            Accept: "application/json",
+                        },
+                    });
+
+                    const data = await response.json();
+
+
+                    if (!data) {
+                        throw new Error("No data returned from the API");
+                    }
+
+                    setAuthData(data);
+                    return data;
+                } catch (error: any) {
+                    console.error(`Attempt ${attempt} failed:`, error);
+
+                    if (attempt < MAX_RETRIES) {
+                        await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY));
+                        return retryFetch(attempt + 1);
+                    } else {
+                        //console.error("Max retries reached. Failed to fetch data.");
+                        setHasError(true);
+                        throw error;
+                    }
+                }
+            };
+
             try {
-                const response = await fetch(`/api/bankId?${queryString}`, {
-                    method: "GET",
-                    headers: {
-                        "Content-Type": "application/json",
-                        'x-api-key': process.env.NEXT_PUBLIC_BANKID_VERIFICATION_API_KEY || "",
-                        Accept: "application/json",
-                    },
-                });
-
-                const data = await response.json();
-
-                console.log('debug - setting AUTH');
-                console.log('debug - (auth) - ', data);
-
-                setAuthData(data);
-            } catch (error: any) {
-                //console.error("Error fetching:", error.message);
-                setHasError(true);
+                await retryFetch();
+            } catch (error) {
+                console.error("Failed to set auth data after retries:", error);
             }
         };
 
@@ -148,6 +178,11 @@ const useBankId = (options?: BankIdOptions) => {
         if (triggerCounter === 0 || !auth?.orderRef) return;
 
         const collectToken = async () => {
+            if (completed) {
+                clearInterval(intervalId);
+                return;
+            }
+
             const params = {
                 params: JSON.stringify({ orderRef: auth.orderRef, skipSetBankIdServerSecret: options?.skipSetBankIdServerSecret }),
                 method: "collect",
@@ -172,9 +207,11 @@ const useBankId = (options?: BankIdOptions) => {
                     setCompleted(true);
                 } else if (data?.status === 'failed' || hasTimedOut || hasError) {
                     clearInterval(intervalId);
+                    setHasError(true);
+                    setHintCode(data.hintCode);
                 }
             } catch (error: any) {
-                //console.error("Error fetching:", error.message);
+                //console.log("Error fetching:", error);
                 setHasError(true);
                 clearInterval(intervalId);
             }
@@ -183,13 +220,13 @@ const useBankId = (options?: BankIdOptions) => {
         const intervalId = setInterval(collectToken, REFRESH_INTERVAL);
 
         return () => clearInterval(intervalId);
-    }, [auth, hasTimedOut, hasError, triggerCounter]);
+    }, [auth, hasTimedOut, completed, hasError, triggerCounter]);
 
     // Start the QR code refresh process
     useEffect(() => {
-        if (triggerCounter === 0 || !auth?.qrStartToken || autoSignIn) return;
+        if (triggerCounter === 0 || !auth?.qrStartToken || !auth.orderRef || autoSignIn) return;
 
-        return QR_CODE.startQRCodeRefresh(auth.qrStartToken, setQRDataString, setHasError, completed, hasTimedOut, hasError);
+        return QR_CODE.startQRCodeRefresh(auth.qrStartToken, auth.orderRef, setQRDataString, setHasError, completed, hasTimedOut, hasError);
     }, [auth, completed, hasTimedOut, hasError, triggerCounter]);
 
     // Generate QR code image
@@ -200,9 +237,9 @@ const useBankId = (options?: BankIdOptions) => {
             .then((url: string) => {
                 setQRCodeImage(url);
             })
-            .catch((err: any) => {
+            .catch((error: any) => {
                 setQRCodeImage("");
-                setHasError(true);
+                //setHasError(true);
                 //console.error("Error generating QR code image:", err);
             });
     }, [qrDataString]);
@@ -212,6 +249,7 @@ const useBankId = (options?: BankIdOptions) => {
         hasTimedOut,
         identification,
         hasError,
+        hintCode,
         retriggerAuth,
         triggerAutoSignIn: () => setAutoSignIn(true),
         autoStartToken: auth?.autoStartToken,
